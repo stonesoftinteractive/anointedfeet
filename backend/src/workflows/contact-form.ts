d5796b89bc09405527fb920aa8b8125e59ebab0e
Charles Tetteh-Quaye <charles.tquaye@stonesoftinteractive.com>
2026-02-09 01:57:49 -0600

    commented out contact workflow

--- backend/src/workflows/contact-form.ts
+++ backend/src/workflows/contact-form.ts
@@ -1,30 +1,30 @@
-import {
-  createWorkflow,
-  WorkflowResponse,
-} from "@medusajs/framework/workflows-sdk";
-import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk";
-import { Modules } from "@medusajs/framework/utils";
+// import {
+//   createWorkflow,
+//   WorkflowResponse,
+// } from "@medusajs/framework/workflows-sdk";
+// import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk";
+// import { Modules } from "@medusajs/framework/utils";
 
-const sendContactFormEmailStep = createStep(
-  "send-contact-form-email",
-  async (input: { to: string; data: any }, { container }) => {
-    const notificationModuleService = container.resolve(Modules.NOTIFICATION);
+// const sendContactFormEmailStep = createStep(
+//   "send-contact-form-email",
+//   async (input: { to: string; data: any }, { container }) => {
+//     const notificationModuleService = container.resolve(Modules.NOTIFICATION);
 
-    const notification = await notificationModuleService.createNotifications({
-      to: input.to,
-      channel: "email",
-      template: "contact-form-template", // The ID in your SendGrid/Resend dash
-      data: input.data,
-    });
+//     const notification = await notificationModuleService.createNotifications({
+//       to: input.to,
+//       channel: "email",
+//       template: "contact-form-template", // The ID in your SendGrid/Resend dash
+//       data: input.data,
+//     });
 
-    return new StepResponse(notification);
-  },
-);
+//     return new StepResponse(notification);
+//   },
+// );
 
-export const contactFormWorkflow = createWorkflow(
-  "contact-form",
-  (input: { to: string; data: any }) => {
-    const result = sendContactFormEmailStep(input);
-    return new WorkflowResponse(result);
-  },
-);
+// export const contactFormWorkflow = createWorkflow(
+//   "contact-form",
+//   (input: { to: string; data: any }) => {
+//     const result = sendContactFormEmailStep(input);
+//     return new WorkflowResponse(result);
+//   },
+// );
